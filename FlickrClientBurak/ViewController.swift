9576acef681dcca76478ec3d7b99136004d3eec4
Burak Gunduz <burakgunduz@burak-macbook-pro.local>
2017-04-22 11:49:00 +0300

    Initial Commit

---
+++ FlickrClientBurak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrClientBurak
+//
+//  Created by Burak Gunduz on 22/04/2017.
+//  Copyright © 2017 Burak Gunduz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
